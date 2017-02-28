eafa69de055e32900fb0a551587b5a03d146893a
cris_ <cris_@DESKTOP-GF2A7MD>
2017-02-28 12:13:45 -0600

    Version 1.0

---
+++ src/nomina/Nomina.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nomina;
+
+/**
+ *
+ * @author cris_
+ */
+public class Nomina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculo ventana = new Calculo();
+        ventana.setVisible(true);
+    }
+    
+}
